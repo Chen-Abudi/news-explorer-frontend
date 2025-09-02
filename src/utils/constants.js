97d3de4a252470b246e619c7b485991589328b73
Grace Chen Abudi <cheezumcali23@gmail.com>
2025-09-02 18:28:25 +0300

    fix(security): Move hardcoded API key to environment variable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.NEWS_EXPLORER_API_KEY;
+const API_KEY = process.env.NEWS_EXPLORER_API_KEY; //
 const PROXY_URL = "https://nomoreparties.co/news/v2/everything";
 const NUMBER_CARDS = 3;
 const NUMBER_ARTICLES = 100;
