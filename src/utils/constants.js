722deff5b7415e2671f56b7e15d4e8aebc331910
Grace Chen Abudi <cheezumcali23@gmail.com>
2025-09-02 18:57:31 +0300

    chore: Clear comments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,4 @@
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
-// console.log("API_KEY:", API_KEY); // debug
 const PROXY_URL = "https://nomoreparties.co/news/v2/everything";
 const NUMBER_CARDS = 3;
 const NUMBER_ARTICLES = 100;
